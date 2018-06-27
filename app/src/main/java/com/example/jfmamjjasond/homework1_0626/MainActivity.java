147a1874a0d22a84997cdfd03a180dd4eed13953
remagart <jfmamjjasond418@gmail.com>
2018-06-27 23:14:29 +0800

    Revert "v1_建立按鈕物件與增加一頁layout給dialog使用"
    This reverts commit f88efcbdf173b1480268de9c72e585250b82d35e.

--- app/src/main/java/com/example/jfmamjjasond/homework1_0626/MainActivity.java
+++ app/src/main/java/com/example/jfmamjjasond/homework1_0626/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.jfmamjjasond.homework1_0626;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
+    Context thisactivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thisactivity = this;
+
+        b1 = (Button)findViewById(R.id.myb1);
+        b1.setOnClickListener(myevent);
+
     }
+
+    View.OnClickListener myevent = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
 }
