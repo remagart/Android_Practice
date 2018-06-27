7509875afba39c3d230ededaca3dd60d0ab6f19f
remagart <40617101+remagart@users.noreply.github.com>
2018-06-27 16:27:23 +0800

    mymovielist_v5_建立main

--- mymovielist/app/src/main/java/com/example/administrator/mymovielist/MainActivity.java
+++ mymovielist/app/src/main/java/com/example/administrator/mymovielist/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.administrator.mymovielist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView mylist;
+    public mymovieadapter myadapter = null;
+    public ArrayList<mymovie> allmovie = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list);
+
+        mylist = (ListView)findViewById(R.id.mylist);
+        allmovie = new ArrayList<mymovie>();
+        allmovie.add(new mymovie(R.mipmap.summer,"夏日1993","2018/6/29","★ 代表西班牙角逐2018奧斯卡最佳外語片..."));
+
+        myadapter = new mymovieadapter(this,allmovie);
+        mylist.setAdapter(myadapter);
+
     }
 }
