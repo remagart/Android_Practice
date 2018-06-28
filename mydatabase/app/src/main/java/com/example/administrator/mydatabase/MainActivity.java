8abf14f42a2733b6709de1abc58c00ae3b12e907
remagart <40617101+remagart@users.noreply.github.com>
2018-06-28 16:22:25 +0800

    mydatabase_v4_創資料庫和建表單，建立類別放方法

--- mydatabase/app/src/main/java/com/example/administrator/mydatabase/MainActivity.java
+++ mydatabase/app/src/main/java/com/example/administrator/mydatabase/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.administrator.mydatabase;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnCreate, btnUpdate, btnDelete, btnQuery;
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     TextView txtTel, txtName, txtEmail;
     Context thisactivity;
 
+    SQLiteDatabase mydb;
+    String table_name="mytable";
+    String sql;
+    String path;
+    MainActivity m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         thisactivity = this;
 
+        m = new MainActivity();
+
         myinit();
+        mylib.myopen(thisactivity,m);
+        mylib.mycreatetable(thisactivity,m);
 
     }
+//    public void myopen(){
+//        path = "/data/data/"+ getPackageName()+"/youaremonkey.db";
+//        mydb = SQLiteDatabase.openOrCreateDatabase(path,null);
+//        Toast.makeText(thisactivity, "HIHI", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    public void mycreatetable(){
+//        sql ="CREATE TABLE IF NOT EXISTS " + table_name +"(_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, email TEXT);";
+//        mydb.execSQL(sql);
+//        Toast.makeText(thisactivity, "baba", Toast.LENGTH_SHORT).show();
+//    }
 
     public void myinit(){
         btnCreate = (Button)findViewById(R.id.myb1);
@@ -52,7 +74,10 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch(v.getId()){
                 case R.id.myb1:
-                    mylib.add(thisactivity);
+                    //mylib.add(thisactivity,m);
+
+                    //Toast.makeText(thisactivity, "zzzz"+m.table_name, Toast.LENGTH_SHORT).show();
+
                     break;
                 case R.id.myb2:
                     break;
--- mydatabase/app/src/main/java/com/example/administrator/mydatabase/mylib.java
+++ mydatabase/app/src/main/java/com/example/administrator/mydatabase/mylib.java
@@ -1,15 +1,26 @@
 package com.example.administrator.mydatabase;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
 
 public class mylib {
 
-    static  void open(Context c){
-        
+    static void myopen(Context c,MainActivity m){
+        m.path = "/data/data/"+ c.getPackageName()+"/youaremonkey.db";
+        m.mydb = SQLiteDatabase.openOrCreateDatabase(m.path,null);
+        Toast.makeText(c, "HIHI", Toast.LENGTH_SHORT).show();
     }
 
-    static void add(Context c){
+    static void mycreatetable(Context c,MainActivity m){
+        m.sql ="CREATE TABLE IF NOT EXISTS " + m.table_name +
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, email TEXT);";
+        m.mydb.execSQL(m.sql);
+        Toast.makeText(c, "baba", Toast.LENGTH_SHORT).show();
+    }
+
+    static void add(Context c,MainActivity m){
         Toast.makeText(c, "HIHIH", Toast.LENGTH_SHORT).show();
+        m.table_name = "nnnnnm";
     }
 }
