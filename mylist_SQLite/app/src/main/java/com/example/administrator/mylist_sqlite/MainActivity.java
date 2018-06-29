37a5fdec8b6b59bb0cc908fb74a57d79e4b6b43c
remagart <40617101+remagart@users.noreply.github.com>
2018-06-29 11:53:40 +0800

    mylist_SQLite_v5_創建menu物件並做menu事件處理，連到修改頁
    要做switch判斷，因為新增和修改都會傳到修改頁

--- mylist_SQLite/app/src/main/java/com/example/administrator/mylist_sqlite/MainActivity.java
+++ mylist_SQLite/app/src/main/java/com/example/administrator/mylist_sqlite/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.mylist_sqlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater myinflater = getMenuInflater();
+        myinflater.inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent();
+        switch (item.getItemId()){
+            case R.id.menuadd:
+                i.setClass(MainActivity.this,edit.class);
+                startActivity(i);
+                break;
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
