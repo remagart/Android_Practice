cbea3205ea9ee4305444ca9677f3740b30875836
remagart <40617101+remagart@users.noreply.github.com>
2018-07-04 12:00:53 +0800

    MyToDoList_v7_建立DBAdapter，建立連結資料庫的方法

--- MyToDoList/app/src/main/java/com/example/administrator/mytodolist/myDBAdapter.java
+++ MyToDoList/app/src/main/java/com/example/administrator/mytodolist/myDBAdapter.java
@@ -1,4 +1,35 @@
 package com.example.administrator.mytodolist;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class myDBAdapter {
+    final static String KEY_myname_of_table = "thisisToDoListTable";
+    final static String KEY_myid = "_id";
+    final static String KEY_mydate = "date";
+    final static String KEY_mytitle = "title";
+    final static String KEY_mycontent = "content";
+
+    SQLiteDatabase mySQLiteDB;
+    myDBHelper mmyDBHelper;
+    Context mycontext;
+    ContentValues myvalies;
+
+    myDBAdapter(Context c){
+        mycontext = c;
+        open();
+    }
+
+    public void open(){
+        mmyDBHelper = new myDBHelper(mycontext);
+        mySQLiteDB = mmyDBHelper.getWritableDatabase();
+    }
+    public void close(){
+        if(mmyDBHelper != null){
+            mySQLiteDB.close();
+        }
+    }
+
+
 }
