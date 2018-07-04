4001103856152f3340542f173645ae5fda50b406
remagart <40617101+remagart@users.noreply.github.com>
2018-07-04 11:09:32 +0800

    MyToDoList_v4_建立listview和它的格式並可從Main連結過去

--- MyToDoList/app/src/main/java/com/example/administrator/mytodolist/MainActivity.java
+++ MyToDoList/app/src/main/java/com/example/administrator/mytodolist/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.administrator.mytodolist;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    Context thisactivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thisactivity = this;
+
     }
 
     @Override
@@ -18,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater myinflater = getMenuInflater();
         myinflater.inflate(R.menu.check,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent();
+        i.setClass(thisactivity,all_content.class);
+        startActivity(i);
+        return super.onOptionsItemSelected(item);
     }
 }
