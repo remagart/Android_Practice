93a43c2d4c41305f1760ca921c073ae3010450be
remagart <40617101+remagart@users.noreply.github.com>
2018-07-05 13:28:06 +0800

    mygesture_v3_放入主程式碼(未完)

--- mygesture/app/src/main/java/com/example/administrator/mygesture/MainActivity.java
+++ mygesture/app/src/main/java/com/example/administrator/mygesture/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.administrator.mygesture;
 
 import android.content.Context;
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView myimg;
     Context thisactivity;
+    ScaleGestureDetector SGD;
+    private Matrix matrix = new Matrix();
+    private float scale = 1f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
     }
     void init(){
         myimg = (ImageView)findViewById(R.id.mainxml_imgview_myimg);
+        SGD =  new ScaleGestureDetector(this,new myScaleListener());
+    }
+
+    class myScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale *= detector.getScaleFactor();
+            scale = Math.max(0.1f, Math.min(scale, 5.0f));
+            matrix.setScale(scale, scale);
+            //myimg.setImageMatrix(matrix);
+            return super.onScale(detector);
+        }
     }
 }
