08d6ff10a745b5741d63c365589334a15be28e1d
remagart <40617101+remagart@users.noreply.github.com>
2018-07-05 13:59:53 +0800

    myspinner_for_sandboxtest_v3_建立Spinner並顯示出來
    準備字串陣列資料
    加入Spinner元件
    使用ArrayAdapter

--- myspinner_for_sandboxtest/app/src/main/java/com/example/administrator/myspinner_for_sandboxtest/MainActivity.java
+++ myspinner_for_sandboxtest/app/src/main/java/com/example/administrator/myspinner_for_sandboxtest/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.administrator.myspinner_for_sandboxtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Spinner myspinner;
+    ArrayAdapter<CharSequence> myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        myAdapter = ArrayAdapter.createFromResource(this,R.array.colorr,android.R.layout.simple_spinner_item);
+        myspinner.setAdapter(myAdapter);
+
     }
 
     void init(){
