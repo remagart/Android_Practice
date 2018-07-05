de76a1a304361be19e623a965837a00896e1289e
remagart <40617101+remagart@users.noreply.github.com>
2018-07-05 13:37:12 +0800

    myspinner_for_sandboxtest_v2_建立layout以及它的初始化

--- myspinner_for_sandboxtest/app/src/main/java/com/example/administrator/myspinner_for_sandboxtest/MainActivity.java
+++ myspinner_for_sandboxtest/app/src/main/java/com/example/administrator/myspinner_for_sandboxtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.administrator.myspinner_for_sandboxtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner myspinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    void init(){
+        myspinner = (Spinner)findViewById(R.id.myspinner);
     }
 }
