a7d28dd9f4baad907837694816713c4b9aa8507f
remagart <40617101+remagart@users.noreply.github.com>
2018-07-05 09:46:58 +0800

    mysensor_v2_建立layout以及它的初始化
    textview和imgview

--- mysensor/app/src/main/java/com/example/administrator/mysensor/MainActivity.java
+++ mysensor/app/src/main/java/com/example/administrator/mysensor/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.administrator.mysensor;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Context thisactivity;
+    ImageView myimg;
+    TextView mytitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thisactivity = this;
+        init();
+        
+
     }
+
+    void init(){
+        myimg = (ImageView)findViewById(R.id.mainxml_imgview_myimg);
+        mytitle = (TextView)findViewById(R.id.mainxml_title);
+
+    }
+
 }
