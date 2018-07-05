2720ffc36690cf61c52036e1d881ce7148da5d9e
remagart <40617101+remagart@users.noreply.github.com>
2018-07-05 11:42:36 +0800

    mygesture_v2_建立layout以及它的初始化

--- mygesture/app/src/main/java/com/example/administrator/mygesture/MainActivity.java
+++ mygesture/app/src/main/java/com/example/administrator/mygesture/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.administrator.mygesture;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView myimg;
+    Context thisactivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thisactivity = this;
+        init();
+
+    }
+    void init(){
+        myimg = (ImageView)findViewById(R.id.mainxml_imgview_myimg);
     }
 }
