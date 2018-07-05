aaec8a339d23c62976ffec5ba0dca7642bdacfdc
remagart <40617101+remagart@users.noreply.github.com>
2018-07-05 10:33:22 +0800

    mysensor_v3_建立Sensor物件，因為指北針需要加速度和磁力
    mysensor_v3:SensorManager lets you access the device's sensors.
    mysensor_v3:TYPE_ACCELEROMETER,TYPE_MAGNETIC_FIELD
    mysensor_v3:在Context類別下SENSOR_SERVICE:Use with "Object getSystemService(String)" to retrieve a SensorManager for accessing sensors.
    mysensor_v3:Sensor getDefaultSensor(int type).Use this method to get the default sensor for a given type.

--- mysensor/app/src/main/java/com/example/administrator/mysensor/MainActivity.java
+++ mysensor/app/src/main/java/com/example/administrator/mysensor/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.administrator.mysensor;
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
     Context thisactivity;
     ImageView myimg;
     TextView mytitle;
+    //mysensor_v3:SensorManager lets you access the device's sensors.
+    SensorManager mySensorManger;
+    //mysensor_v3:TYPE_ACCELEROMETER,TYPE_MAGNETIC_FIELD
+    Sensor my_acc_Sensor,my_mag_Sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +25,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         thisactivity = this;
         init();
-        
+
 
     }
 
     void init(){
         myimg = (ImageView)findViewById(R.id.mainxml_imgview_myimg);
         mytitle = (TextView)findViewById(R.id.mainxml_title);
+        //mysensor_v3:在Context類別下SENSOR_SERVICE:
+        //Use with "Object getSystemService(String)" to retrieve a SensorManager for accessing sensors.
+        mySensorManger = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //mysensor_v3:Sensor getDefaultSensor(int type)
+        //Use this method to get the default sensor for a given type.
+        my_acc_Sensor = mySensorManger.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        my_mag_Sensor = mySensorManger.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
