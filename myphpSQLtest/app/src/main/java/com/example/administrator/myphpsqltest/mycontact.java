86afdf86687ef01aa6c1f285ed71bccc5b87e11e
remagart <40617101+remagart@users.noreply.github.com>
2018-07-06 13:38:05 +0800

    myphpSQLtest_v2_創建listview和自己的物件宣告
    private Bitmap myimg;
        private String myname;
        private String mytel;
        private String myemail;
        private String mybirth;

---
+++ myphpSQLtest/app/src/main/java/com/example/administrator/myphpsqltest/mycontact.java
@@ -0,0 +1,51 @@
+package com.example.administrator.myphpsqltest;
+
+import android.graphics.Bitmap;
+
+public class mycontact {
+    private Bitmap myimg;
+    private String myname;
+    private String mytel;
+    private String myemail;
+    private String mybirth;
+
+    public Bitmap getMyimg() {
+        return myimg;
+    }
+
+    public void setMyimg(Bitmap myimg) {
+        this.myimg = myimg;
+    }
+
+    public String getMyname() {
+        return myname;
+    }
+
+    public void setMyname(String myname) {
+        this.myname = myname;
+    }
+
+    public String getMytel() {
+        return mytel;
+    }
+
+    public void setMytel(String mytel) {
+        this.mytel = mytel;
+    }
+
+    public String getMyemail() {
+        return myemail;
+    }
+
+    public void setMyemail(String myemail) {
+        this.myemail = myemail;
+    }
+
+    public String getMybirth() {
+        return mybirth;
+    }
+
+    public void setMybirth(String mybirth) {
+        this.mybirth = mybirth;
+    }
+}
