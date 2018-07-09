d22e64038267de28bf5c7a5a05dcebf7d5af77e8
remagart <40617101+remagart@users.noreply.github.com>
2018-07-09 15:40:54 +0800

    myphpSQLtest_v12_寫成網頁可以讀的格式吧?!
    - 增加button
    - 一堆wrirebyte
    
    20180709

--- myphpSQLtest/app/src/main/java/com/example/administrator/myphpsqltest/edit.java
+++ myphpSQLtest/app/src/main/java/com/example/administrator/myphpsqltest/edit.java
@@ -1,13 +1,50 @@
 package com.example.administrator.myphpsqltest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class edit extends AppCompatActivity {
+
+    Context thisactivity;
+    EditText newname,newtel,newbirth,newmail;
+    Button btn_OK;
+    String[] all;
+    myinsert mmyinsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        thisactivity = this;
+        all = new String[5];
+        init();
+
+        btn_OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(thisactivity, "HIHI", Toast.LENGTH_SHORT).show();
+                mmyinsert = new myinsert(thisactivity,all);
+                mmyinsert.execute("https://empurpled-nomenclat.000webhostapp.com/php/insert.php");
+            }
+        });
+
+
+
+    }
+    void init(){
+        newname = (EditText)findViewById(R.id.editxml_name);
+        newtel = (EditText)findViewById(R.id.editxml_tel);
+        newbirth = (EditText)findViewById(R.id.editxml_birth);
+        newmail = (EditText)findViewById(R.id.editxml_mail);
+        btn_OK = (Button)findViewById(R.id.editxml_btn_confirm);
+        all[1] = newname.getText().toString();
+        all[2] = newtel.getText().toString();
+        all[3] = newmail.getText().toString();
+        all[4] = newbirth.getText().toString();
     }
 }
--- myphpSQLtest/app/src/main/java/com/example/administrator/myphpsqltest/myinsert.java
+++ myphpSQLtest/app/src/main/java/com/example/administrator/myphpsqltest/myinsert.java
@@ -2,7 +2,9 @@ package com.example.administrator.myphpsqltest;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -11,9 +13,15 @@ import java.net.URL;
 public class myinsert extends AsyncTask<String,Void,String> {
 
     Context thisactivity;
+    String crlf = "\r\n";
+    String twoHyphens = "--";
+    String boundary =  "*****";
+    int index;
+    String[] data;
 
-    public myinsert(Context c) {
+    public myinsert(Context c,String[] temp_data) {
         thisactivity = c;
+        data = temp_data;
     }
 
     @Override
@@ -23,8 +31,46 @@ public class myinsert extends AsyncTask<String,Void,String> {
             u = new URL(temp[0]);
             HttpURLConnection conn = (HttpURLConnection) u.openConnection();
             conn.setRequestMethod("POST");
-            conn.connect();
+            //conn.connect();
             conn.setDoOutput(true);
+
+            DataOutputStream request = new DataOutputStream(conn.getOutputStream());
+////////////////////////////////////////////////
+            request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+            request.writeBytes("Content-Disposition: form-data; name=\"ContactID\"" + "\"" + this.crlf);
+            request.writeBytes(this.crlf);
+            Log.i("index=====>",String.valueOf(index));
+            request.writeBytes(String.valueOf(index));
+            request.writeBytes(this.crlf);
+
+            request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+            request.writeBytes("Content-Disposition: form-data; name=\"Name\"" + "\"" + this.crlf);
+            request.writeBytes(this.crlf);
+            request.writeBytes(data[1]);
+            request.writeBytes(this.crlf);
+            request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+            request.writeBytes("Content-Disposition: form-data; name=\"Phone\"" + "\"" + this.crlf);
+            request.writeBytes(this.crlf);
+            request.writeBytes(data[2]);
+            request.writeBytes(this.crlf);
+            request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+            request.writeBytes("Content-Disposition: form-data; name=\"Email\"" + "\"" + this.crlf);
+            request.writeBytes(this.crlf);
+            request.writeBytes(data[3]);
+            request.writeBytes(this.crlf);
+            request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+            request.writeBytes("Content-Disposition: form-data; name=\"Birthday\"" + "\"" + this.crlf);
+            request.writeBytes(this.crlf);
+            request.writeBytes(data[4]);
+            request.writeBytes(this.crlf);
+            request.writeBytes(this.twoHyphens + this.boundary + this.twoHyphens + this.crlf);
+
+            request.flush();
+            request.close();
+
+            conn.connect();
+
+            
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
