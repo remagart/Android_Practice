5583a212a525baa353f1c49bb4e633ff3e40fc9b
remagart <jfmamjjasond418@gmail.com>
2018-12-13 01:01:02 +0800

    [myphptestsql][Add]新增dbinsert add DB資料
    - 有增加Activity，記得AndroidManifest.xml要定義
    - ==========================================
    - 要做bar的新增按鈕，所以增加menu資料夾
    - 要將menu放進MainActivity需覆寫以下兩個方法
    - onCreateOptionsMenu(Menu menu)
    - 使用inflater將menu設計的按鈕放進來
    
    - onOptionsItemSelected(MenuItem item)
    - 點選按鈕時的事件觸發，這邊使用Intent
    - ==========================================
    - 新增資料到資料庫有兩個重點：連線 和 封包格式
    - 1. 連線
    - 連線到新增的php
    - 將資料(封包)寫入內存緩衝區
    - 迫使資料緩衝區的資料寫到輸出流：flush()、close()
    - 建立與server的tcp連線：myconn.connect();
    - 發送http請求：InputStream input = myconn.getInputStream();
    - 參考：
    - https://blog.csdn.net/wzumath/article/details/6939707
    -
    - 2. 封包格式
    - TWOHYPHENS,BOUNDARY,CRLF真的要分開寫...
    - 差一個都會fail
    - 因為使用Content-Disposition: form-data
    - 所以分隔線和dash線很重要，換行也是
    - 參考：
    - https://www.jianshu.com/p/e810d1799384
    - =====================================================
    - 參考基本格式(name/filename後要引號，因commit才拿掉):
    
    --分隔符(boundary)[換行]
    Content-Disposition: form-data; name=參數名[換行]
    [換行]
    參數值[換行]
    --分隔符(boundary)[換行]
    Content-Disposition: form-data; name=圖片名; filename=圖片文件名[換行]
    Content-Type: 類型[換行]
    [換行]
    圖片文件的二進制内容[換行]
    --分隔符(boundary)--
    
    20181213

--- myphptestsql/app/src/main/java/com/example/jfmamjjasond/myphptestsql/MainActivity.java
+++ myphptestsql/app/src/main/java/com/example/jfmamjjasond/myphptestsql/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.jfmamjjasond.myphptestsql;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -32,5 +36,22 @@ public class MainActivity extends AppCompatActivity {
         myadapter = new list_adapter(thisactivity,list_info);
         mylistview.setAdapter(myadapter);
 
+    }
+
+    //覆寫方法，因為要將menu放進來
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menu_inflater = getMenuInflater();
+        menu_inflater.inflate(R.menu.add,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //覆寫方法，因為要將menu放進來
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent();
+        i.setClass(thisactivity,addcontact.class);
+        startActivity(i);
+        return super.onOptionsItemSelected(item);
     }
 }
